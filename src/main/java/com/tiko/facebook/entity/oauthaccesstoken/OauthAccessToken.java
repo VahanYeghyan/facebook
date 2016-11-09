e52d9ac657807e84ed155e53ddc548c5829f85c8
Vazgen Danielyan <vazgen.danielyan@sflpro.com>
2016-11-09 21:58:42 +0400

    Added getters and setters

--- src/main/java/com/tiko/facebook/entity/oauthaccesstoken/OauthAccessToken.java
+++ src/main/java/com/tiko/facebook/entity/oauthaccesstoken/OauthAccessToken.java
@@ -1,6 +1,9 @@
 package com.tiko.facebook.entity.oauthaccesstoken;
 
 import com.tiko.facebook.entity.AbstractDomainEntityModel;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -41,4 +44,114 @@ public class OauthAccessToken extends AbstractDomainEntityModel {
     @Column(name = "refresh_token")
     private String refreshToken;
 
+
+    /*Getters and setters*/
+    public String getTokenId() {
+        return tokenId;
+    }
+
+    public void setTokenId(final String tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    public byte[] getToken() {
+        return token;
+    }
+
+    public void setToken(final byte[] token) {
+        this.token = token;
+    }
+
+    public String getAuthenticationId() {
+        return authenticationId;
+    }
+
+    public void setAuthenticationId(final String authenticationId) {
+        this.authenticationId = authenticationId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(final String userName) {
+        this.userName = userName;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(final String clientId) {
+        this.clientId = clientId;
+    }
+
+    public byte[] getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(final byte[] authentication) {
+        this.authentication = authentication;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(final String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        final OauthAccessToken rhs = (OauthAccessToken) obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(this.getTokenId(), rhs.getTokenId())
+                .append(this.getToken(), rhs.getToken())
+                .append(this.getAuthenticationId(), rhs.getAuthenticationId())
+                .append(this.getUserName(), rhs.getUserName())
+                .append(this.getClientId(), rhs.getClientId())
+                .append(this.getAuthentication(), rhs.getAuthentication())
+                .append(this.getRefreshToken(), rhs.getRefreshToken())
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(getTokenId())
+                .append(getToken())
+                .append(getAuthenticationId())
+                .append(getUserName())
+                .append(getClientId())
+                .append(getAuthentication())
+                .append(getRefreshToken())
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("tokenId", getTokenId())
+                .append("token", getToken())
+                .append("authenticationId", getAuthenticationId())
+                .append("userName", getUserName())
+                .append("clientId", getClientId())
+                .append("authentication", getAuthentication())
+                .append("refreshToken", getRefreshToken())
+                .toString();
+    }
 }
--- src/main/java/com/tiko/facebook/entity/oauthrefreshtoken/OauthRefreshToken.java
+++ src/main/java/com/tiko/facebook/entity/oauthrefreshtoken/OauthRefreshToken.java
@@ -1,6 +1,9 @@
 package com.tiko.facebook.entity.oauthrefreshtoken;
 
 import com.tiko.facebook.entity.AbstractDomainEntityModel;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -29,4 +32,71 @@ public class OauthRefreshToken extends AbstractDomainEntityModel {
 
     @Column(name = "authentication")
     private byte[] authentication;
+
+
+    /*Getters and setters*/
+    public String getTokenId() {
+        return tokenId;
+    }
+
+    public void setTokenId(final String tokenId) {
+        this.tokenId = tokenId;
+    }
+
+    public byte[] getToken() {
+        return token;
+    }
+
+    public void setToken(final byte[] token) {
+        this.token = token;
+    }
+
+    public byte[] getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(final byte[] authentication) {
+        this.authentication = authentication;
+    }
+
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        final OauthRefreshToken rhs = (OauthRefreshToken) obj;
+        return new EqualsBuilder()
+                .appendSuper(super.equals(obj))
+                .append(this.getTokenId(), rhs.getTokenId())
+                .append(this.getToken(), rhs.getToken())
+                .append(this.getAuthentication(), rhs.getAuthentication())
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .appendSuper(super.hashCode())
+                .append(getTokenId())
+                .append(getToken())
+                .append(getAuthentication())
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .appendSuper(super.toString())
+                .append("tokenId", getTokenId())
+                .append("token", getToken())
+                .append("authentication", getAuthentication())
+                .toString();
+    }
 }
